93136687d3ed39147e53984a7c36a9a697b1b32c
Vladimirs Anufrijevs <vladimirs.anufrijevs@gmail.com>
2024-11-03 09:59:06 +0200

    Add 2 homework result

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,55 @@
 public class Main {
     public static void main(String[] args) {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend/7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var firstBoxerMass = 78.2;
+        var secondBoxerMass = 82.7;
+        var totalMass = firstBoxerMass + secondBoxerMass;
+        System.out.println(totalMass);
+        var difference = secondBoxerMass - firstBoxerMass;
+        System.out.println(difference);
+        var remainingMass = secondBoxerMass % firstBoxerMass;
+        System.out.println(remainingMass);
+
+        var totalEmployeeAmount = 640/8;
+        System.out.println("Всего работников в компании " + totalEmployeeAmount + " человек");
+        totalEmployeeAmount = totalEmployeeAmount + 94;
+        var totalWorkHours = totalEmployeeAmount * 8;
+        System.out.println("Если в компании работает " + totalEmployeeAmount + ", то всего " + totalWorkHours + " часов работы может быть поделено между сотрудниками");
     }
 }
